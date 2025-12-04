1adf32f583e59903cd8ec2c8af03d65d79fe18f5
Odko <odgerelariunbold719@gmailc.com>
2025-12-05 00:43:24 +0800

    New page and router change

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import './globals.css';
+
+export default function NotFound() {
+  return (
+    <div className="bg-gray-100 h-screen w-screen flex flex-col justify-center items-center">
+        <h1 className="text-8xl font-bold text-gray-800">247</h1>
+        <p className="text-4xl font-medium text-gray-800">Havent implemented yet</p>
+        <a href="/" className="mt-4 text-xl text-blue-600 hover:underline">Lets go back to home page</a>
+    </div>
+  );
+}
