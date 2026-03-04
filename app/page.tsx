376779645b785e22ba2cc9f3da26afd0cf0d5bf1
Odko <odgerelariunbold719@gmailc.com>
2026-03-04 22:44:44 +0800

    New button added

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,15 @@ export default function Home() {
       </div>
 
       <div className="pb-10 flex items-center justify-center gap-8">
+        <Link href="https://open.spotify.com/playlist/08M2SMi0f4JRp16SXTNvbv?si=uwlWOLePSGmh_Q3bFVE4tQ">
+          <button className="px-6 py-3 rounded-2xl bg-sky-500 text-white shadow-lg active:scale-95 transition">
+            Our playlist
+          </button>
+        </Link>
+      </div>
+
+
+      <div className="pb-10 flex items-center justify-center gap-8">
         <Link href="/invitation">
           <button className="px-6 py-3 rounded-2xl bg-black text-white shadow-lg active:scale-95 transition">
             Ready?
